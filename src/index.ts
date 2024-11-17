c26d9cbc2ef0f779b16f0aa0ee9089e850d7475b
Umut Aydin <aydinumut82@gmail.com>
2024-11-17 23:28:04 +0300

    in line documentation added to CloudomiumLambda

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,46 @@ export type Handler<CE = unknown, CC = unknown, CR = unknown> = (event: CE, cont
 export type ErrorHandler<CR = unknown> = (err: Error) => Promise<CR>
 export type Middleware<CE = unknown, CC = unknown, CR = unknown> = (event: CE, context: CC, response?: CR) => Promise<CR | void>
 
+/**
+ * Lambda organizer with middleware support
+ * @class CloudomiumLambda
+ * @template CE - Cloud Event type
+ * @template CC - Context type
+ * @template CR - Response type
+ * @example new CloudomiumLambda<ApiGatewayProxyEventV2, Context, ApiGatewayProxyResultV2>()
+ */
 export class CloudomiumLambda<CE = unknown, CC = unknown, CR = unknown> {
     protected onBefore: Array<{ middleware: Middleware<CE, CC, CR>; onError?: ErrorHandler<CR> }> = []
     protected onAfter: Array<{ middleware: Middleware<CE, CC, CR>; onError?: ErrorHandler<CR> }> = []
 
+    /**
+     * Adds a middleware to be executed before the handler
+     * @param {Middleware<CE, CC, CR>} middleware - Middleware to be executed
+     * @param {ErrorHandler<CR>} [onError] - Error handler for the middleware
+     * @returns {CloudomiumLambda<CE = unknown, CC = unknown, CR = unknown>}
+    */
     before(middleware: Middleware<CE, CC, CR>, onError?: ErrorHandler<CR>): this {
         this.onBefore.push({ middleware, onError })
         return this
     }
 
+    /**
+     * Adds a middleware to be executed after the handler
+     * @param {Middleware<CE, CC, CR>} middleware - Middleware to be executed
+     * @param {ErrorHandler<CR>} [onError] - Error handler for the middleware
+     * @returns {CloudomiumLambda<CE = unknown, CC = unknown, CR = unknown>}
+    */
     after(middleware: Middleware<CE, CC, CR>, onError?: ErrorHandler<CR>): this {
         this.onAfter.push({ middleware, onError })
         return this
     }
 
+    /**
+     * Assigns the handler function
+     * @param {Handler<CE, CC, CR>} handler - Handler to be executed
+     * @param {ErrorHandler<CR>} [onError] - Error handler for the handler
+     * @returns {Handler<CE, CC, CR>}
+    */
     execute(handler: Handler<CE, CC, CR>, onError?: ErrorHandler<CR>): Handler<CE, CC, CR> {
         return async (event: CE, context: CC): Promise<CR> => {
             for (const { middleware, onError } of this.onBefore) {
