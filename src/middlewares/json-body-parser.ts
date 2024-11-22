e624da68435d3a516101b21672d89b1be8128a8b
Umut Aydin <aydinumut82@gmail.com>
2024-11-22 20:36:04 +0300

    json body parser middleware improved

--- src/middlewares/json-body-parser.ts
+++ src/middlewares/json-body-parser.ts
@@ -1,6 +1,6 @@
 import { unzipSync } from 'zlib'
 
-export interface JsonMiddlewareConfig {
+export interface JsonBodyParserMiddlewareConfig {
     base64?: boolean
     compressed?: boolean
 }
@@ -11,7 +11,7 @@ export interface JsonMiddlewareConfig {
  * @param [config.base64] A boolean to indicate if the body is base64 encoded.
  * @returns A middleware function that parses the request body as JSON.
  */
-export default function (config: JsonMiddlewareConfig = {}) {
+export default function (config: JsonBodyParserMiddlewareConfig = {}) {
     return async (event: any, _context: any): Promise<void> => {
         let body = config.base64 ? Buffer.from(event.body, 'base64') : event.body || '{}'
         if (config.compressed) body = unzipSync(body)
